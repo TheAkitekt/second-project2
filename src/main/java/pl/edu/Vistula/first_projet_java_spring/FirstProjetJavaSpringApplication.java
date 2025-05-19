cbb5388eadf106127cdbe57e9f8ac89f96394e9d
elie <engoymu1@stu.vistula.edu.pl>
2025-05-20 00:12:56 +0200

    Initial commit

---
+++ src/main/java/pl/edu/Vistula/first_projet_java_spring/FirstProjetJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.Vistula.first_projet_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjetJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjetJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/Vistula/first_projet_java_spring/FirstProjetJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.Vistula.first_projet_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjetJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
